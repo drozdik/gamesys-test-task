71ffa9d4bbab1cfc6121e329874e56417c5a557c
vladimir <v_drozdik@hotmail.com>
2018-12-11 14:17:26 +0200

    Setup spring boot project from initializer.

---
+++ src/test/java/gamesys/gamesystesttask/GamesysTestTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package gamesys.gamesystesttask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamesysTestTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
